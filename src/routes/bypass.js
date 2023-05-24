f150ae5f0f192ecd22176eed296420c05ea5a16a
Satendra Sahani <satendrasahani19@gmail.com>
2023-05-23 21:34:12 -1200

    controller and  model

--- src/routes/bypass.js
+++ src/routes/bypass.js
@@ -2,7 +2,7 @@ const express=require("express");
 const { login } = require("../controllers/bypass");
 const router=express.Router();
 router.post("/login",login)
-router.post("/test",(req,res)=>{
+router.get("/test",(req,res)=>{
     res.status(2000).json({msg:"Login Successfully"})
 })
 module.exports=router;
